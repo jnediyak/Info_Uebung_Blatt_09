5bf9256f24fc37c75d9b6bb8b1ce2f3cd728e215
jnediyak <jnediyak@smail.uni-koeln.de>
2025-12-18 20:50:24 +0100

    fertige Hausaufgaben

--- h2/Bus.java
+++ h2/Bus.java
@@ -3,46 +3,59 @@ package h2;
 import java.util.*;
 
 public class Bus {
-	
+
 	ArrayList<Passenger> passengers;
-	
+
 	public Bus() {
 		passengers = new ArrayList<>();
 	}
-	
+
 	public void enterBus(Passenger p) {
 		passengers.add(p);
-		
+
 	}
-	public void exitBus() {
-		for(Passenger p : passengers)
-			if(p.planned == p.visited) passengers.remove(p);
+
+	private void exitBus() {
+		for (int i = passengers.size() - 1; i >= 0; i--) {
+			if (passengers.get(i).planned == passengers.get(i).visited)
+				passengers.remove(i);
+		}
 	}
+
 	public void nextStop(Passenger[] boarding) {
-		for(Passenger p : passengers) p.visited ++;
+		for (Passenger p : passengers)
+			p.visited++;
 		exitBus();
-		for(Passenger p : boarding) {
+		for (Passenger p : boarding) {
 			enterBus(p);
 		}
 	}
+
 	public void nextStop() {
-		for(Passenger p : passengers) p.visited ++;
+		for (Passenger p : passengers)
+			p.visited++;
 		exitBus();
 	}
+
 	public ArrayList<Passenger> findPassengersWithoutTickets() {
 		ArrayList<Passenger> ticketless = new ArrayList<>();
-		for(Passenger p : passengers) {
-			if(!p.ticket) {
+		for (Passenger p : passengers) {
+			if (!p.ticket)
 				ticketless.add(p);
-				passengers.remove(p);
-			}
 		}
+		for (Passenger p : ticketless)
+			passengers.remove(p);
 		return ticketless;
 	}
+
 	public void transferPassengers(Bus otherBus, String[] passengerNames) {
-		for(int i = 0; i < passengers.size(); i++) {
-			for(String n : passengerNames) {
-				if(passengers.get(i).name == n) //Andersrum!!!! forschleifen
+		for (int i = 0; i < passengers.size(); i++) {
+			for(String name : passengerNames) {
+				if(passengers.get(i).name == name) {
+					otherBus.enterBus(passengers.get(i));
+					passengers.remove(i);
+					i--;
+				}
 			}
 		}
 	}
--- h2/H2_main.java
+++ h2/H2_main.java
@@ -1,13 +1,11 @@
 package h2;
 
-import java.util.*;
+
 
 public class H2_main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> i = new ArrayList<>();
 		
-		System.out.println(i.size());
 	}
 
 }
